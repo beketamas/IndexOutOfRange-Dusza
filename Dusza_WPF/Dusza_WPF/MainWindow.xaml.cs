63c7e5cd3cca87792efaa80be58bb66716af8fe6
BT <72067154+beketamas@users.noreply.github.com>
2025-02-10 10:22:43 +0100

    UI progress

--- Dusza_WPF/Dusza_WPF/MainWindow.xaml.cs
+++ Dusza_WPF/Dusza_WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using DuszaProg_IndexOutOfRange;
+using System.Collections.ObjectModel;
 using System.Reflection;
 using System.Text;
 using System.Windows;
@@ -20,83 +21,160 @@ namespace Dusza_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        ObservableCollection<Button> gepek = new ObservableCollection<Button>();
+        ObservableCollection<SzamitogepConfig> gepek = [];
+        readonly List<SzamitogepConfig> klaszter = [];
+        readonly Dictionary<SzamitogepConfig, Line> lines = [];
+        private Point _initialMousePosition;
         public MainWindow()
         {
             InitializeComponent();
-
             GenerateButtons();
             lvGepek.ItemsSource = gepek;
         }
 
         private void GenerateButtons()
         {
-            for (int i = 1; i <= 5; i++)
-            {
-                Button btn = new Button
-                {
-                    Content = $"Button {i}",
-                    Width = 100,
-                    Height = 30,
-                    Margin = new Thickness(5),
-                    Tag = i
-                };
+            var gep1 = new SzamitogepConfig(5000, 6000, "szamitogep1");
+            gep1.PreviewMouseMove += Button_PreviewMouseMove;
+            gep1.PreviewMouseDown += Button_PreviewMouseDown; 
+            
+            var gep2 = new SzamitogepConfig(5000, 6000, "szamitogep2");
+            gep2.PreviewMouseMove += Button_PreviewMouseMove;
+            gep2.PreviewMouseDown += Button_PreviewMouseDown;
 
-                btn.PreviewMouseMove += Button_PreviewMouseMove;
-                gepek.Add(btn);
-            }
+            gepek.Add(gep1);
+            gepek.Add(gep2);
+        }
+
+        private void Button_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is SzamitogepConfig button)
+                _initialMousePosition = e.GetPosition(button);
         }
 
         private void Button_PreviewMouseMove(object sender, MouseEventArgs e)
         {
-            if (e.LeftButton == MouseButtonState.Pressed && sender is Button button)
-                DragDrop.DoDragDrop(button, button, DragDropEffects.Move);
-        }
+            if (sender is SzamitogepConfig gep && e.LeftButton == MouseButtonState.Pressed)
+            {
+                var currentPosition = e.GetPosition(gep);
+                var distanceMoved = (_initialMousePosition - currentPosition).Length;
 
+                if (distanceMoved > 5)
+                    DragDrop.DoDragDrop(gep, gep, DragDropEffects.Move);
+            }
+        }
 
         private void CanvasButton_PreviewMouseMove(object sender, MouseEventArgs e)
         {
-            if (e.LeftButton == MouseButtonState.Pressed && sender is Button button)
-                DragDrop.DoDragDrop(button, button, DragDropEffects.Move);
+            if (e.LeftButton == MouseButtonState.Pressed && sender is SzamitogepConfig button)
+            {
+                var currentPosition = e.GetPosition(button);
+                var distanceMoved = (_initialMousePosition - currentPosition).Length;
+
+                if (distanceMoved > 5)
+                    DragDrop.DoDragDrop(button, button, DragDropEffects.Move);
+            }
         }
 
         private void ListView_DragOver(object sender, DragEventArgs e) => e.Effects = DragDropEffects.Move;
 
         private void Canvas_Drop(object sender, DragEventArgs e)
         {
-
-            if (e.Data.GetData(typeof(Button)) is Button button)
+            if (e.Data.GetData(typeof(SzamitogepConfig)) is SzamitogepConfig gep)
             {
-                gepek.Remove(button);
-                if (VisualTreeHelper.GetParent(button) is ContentPresenter currentParent)
+                if (lines.Any(x => x.Key == gep))
+                {
+                    cKlaszter.Children.Remove(lines.Where(x => x.Key == gep).First().Value);                    
+                    lines.Remove(gep);
+                }
+
+                gepek.Remove(gep);
+                if (VisualTreeHelper.GetParent(gep) is ContentPresenter currentParent)
                     currentParent.Content = null;
 
-
                 Point dropPosition = e.GetPosition(sender as Canvas);
-                Canvas.SetLeft(button, dropPosition.X);
-                Canvas.SetTop(button, dropPosition.Y);
+                Canvas.SetLeft(gep, dropPosition.X);
+                Canvas.SetTop(gep, dropPosition.Y);
 
+                if (sender is Canvas klaszter && !klaszter.Children.Contains(gep))
+                {
+                    klaszter.Children.Add(gep);
+                    this.klaszter.Add(gep);
+                }
 
-                if (sender is Canvas canvas && !canvas.Children.Contains(button))
-                    canvas.Children.Add(button);
-
-                button.PreviewMouseMove -= Button_PreviewMouseMove;
-                button.PreviewMouseMove += CanvasButton_PreviewMouseMove;
+                gep.PreviewMouseMove -= Button_PreviewMouseMove;
+                gep.PreviewMouseMove += CanvasButton_PreviewMouseMove;
+                CreateLine(gep);
+                
             }
+        }
+
+        private void CreateLine(SzamitogepConfig gep)
+        {
+            Line dynamicLine = new()
+            {
+                Stroke = Brushes.Black,
+                StrokeThickness = 2
+            };
+
+            dynamicLine.SetBinding(Line.X1Property, new Binding
+            {
+                Source = gep,
+                Path = new PropertyPath("(Canvas.Left)"),
+            });
+
+            dynamicLine.SetBinding(Line.Y1Property, new Binding
+            {
+                Source = gep,
+                Path = new PropertyPath("(Canvas.Top)")
+            });
+
+            dynamicLine.SetBinding(Line.X2Property, new Binding
+            {
+                Source = btnCenter,
+                Path = new PropertyPath("(Canvas.Left)")
+            });
+
+            dynamicLine.SetBinding(Line.Y2Property, new Binding
+            {
+                Source = btnCenter,
+                Path = new PropertyPath("(Canvas.Top)")
+            });
+            cKlaszter.Children.Add(dynamicLine);
+
+            dynamicLine.Loaded += (sender, e) =>
+            {
+                double gepCenterX = Canvas.GetLeft(gep) + gep.ActualWidth / 2;
+                double gepCenterY = Canvas.GetTop(gep) + gep.ActualHeight / 2;
+
+                double btnCenterX = Canvas.GetLeft(btnCenter) + btnCenter.ActualWidth / 2;
+                double btnCenterY = Canvas.GetTop(btnCenter) + btnCenter.ActualHeight / 2;
+
+                dynamicLine.X1 = gepCenterX;
+                dynamicLine.Y1 = gepCenterY;
+                dynamicLine.X2 = btnCenterX;
+                dynamicLine.Y2 = btnCenterY;
+            };
+            lines.Add(gep, dynamicLine);
         }
 
         private void ListView_Drop(object sender, DragEventArgs e)
         {
-            if (e.Data.GetData(typeof(Button)) is Button button)
+            if (e.Data.GetData(typeof(SzamitogepConfig)) is SzamitogepConfig gep && !gepek.Contains(gep))
             {
+                if (lines.Any(x => x.Key == gep))
+                {
+                    cKlaszter.Children.Remove(lines.Where(x => x.Key == gep).First().Value);
+                    lines.Remove(gep);
+                }
 
-                gepek.Add(button);
-                cKlaszter.Children.Remove(button);
+                gepek.Add(gep);
+                cKlaszter.Children.Remove(gep);
 
-
-                button.PreviewMouseMove -= CanvasButton_PreviewMouseMove;
-                button.PreviewMouseMove += Button_PreviewMouseMove;
+                gep.PreviewMouseMove -= CanvasButton_PreviewMouseMove;
+                gep.PreviewMouseMove += Button_PreviewMouseMove;
             }
         }
+
     }
 }
---
+++ Dusza_WPF/Dusza_WPF/Models/SzamitogepConfig.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DuszaProg_IndexOutOfRange
+{
+    internal class SzamitogepConfig : Button
+    {
+        string eleres;
+        int millimag;
+        int memoria;
+        private List<string> programPeldanyAzonositok = [];
+
+        public SzamitogepConfig(int millimag, int memoria, string eleres)
+        {
+            this.millimag = millimag;
+            this.memoria = memoria;
+            this.eleres = eleres;
+            Content = eleres;
+            Click += (s, e) =>
+            {
+                if (s is SzamitogepConfig gep)
+                    MessageBox.Show(gep.KiIratas());
+            };
+            Width = 150;
+            Height = 80;
+        }
+
+        public int Millimag { get => millimag; set => millimag = value; }
+        public int Memoria { get => memoria; set => memoria = value; }
+        public string Eleres { get => eleres; set => eleres = value; }
+        public List<string> ProgramPeldanyAzonositok { get => programPeldanyAzonositok; set => programPeldanyAzonositok = value; }
+        public string KiIratas() => $"{millimag}\n{memoria}";
+
+        
+    }
+}
